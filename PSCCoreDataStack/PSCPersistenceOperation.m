34c88ba65151ffd20892c9f4380de61064350bf0
Matthias Tretter <matthias.tretter@gmail.com>
2013-05-19 20:20:58 +0200

    Since this context is a private queue context, wrap the save method in a performBlockAndWait call

--- PSCCoreDataStack/PSCPersistenceOperation.m
+++ PSCCoreDataStack/PSCPersistenceOperation.m
@@ -61,15 +61,17 @@ static dispatch_queue_t _psc_persistence_queue = NULL;
 
     dispatch_async(psc_persistence_queue(), ^{
         NSManagedObjectContext *localContext = [parentContext newChildContextWithConcurrencyType:NSPrivateQueueConcurrencyType];
-        NSError *error = nil;
 
         if (block != nil) {
             block(localContext);
         }
 
-        if (![localContext save:&error]) {
-            PSCCDLog(@"Error persisting local context in PSCPersistenceAction: %@", error);
-        }
+        [localContext performBlockAndWait:^{
+            NSError *error = nil;
+            if (![localContext save:&error]) {
+                PSCCDLog(@"Error persisting local context in PSCPersistenceAction: %@", error);
+            }
+        }];
 
         if (completion != nil) {
             dispatch_async(dispatch_get_main_queue(), completion);
